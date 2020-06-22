952264e707cd38caba1e3d05151e7b006753295f
ksea55 <wangyang198118@126.com>
2020-06-22 21:54:14 +0800

    springboot 2.2version 学习记录

---
+++ 4springboot2-started/src/main/java/ksea/springboot2/started/StartedApplication.java
@@ -0,0 +1,14 @@
+package ksea.springboot2.started;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartedApplication.class, args);
+    }
+
+}
